3e5dfa484e0322ea554bddf009888f42924c2d9c
Rachel Budzinski <rbudzinski@arizona.edu>
2025-10-24 17:13:24 -0400

    Create hello.py

---
+++ scripts/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+GIST 604B - Open Source GIS
+Module 1 Assignment 3: GitHub Repository Management
+
+Student: Rachel Budzinski
+Date: 10/24/2025
+University of Arizon
+"""
+
+def main():
+    """Simple hello world function for GIST 604B"""
+    print("Hello from GIST 604B!")
+    print(f"Student: Rachel Budzinski")
+    print("Learning open source GIS workflows with GitHub")
+    print("University of Arizona coordinates: 32.2319° N, 110.9501° W")
+    
+    # Add your own message here!
+    print("My interest  in GIS stems from a desire to use spatial data to improve emergency response and community safety. As a firefighter and drone pilot, I’m especially focused on integrating aerial data and GIS to support real-time decision-making during emergency situations as well as the planning to prevent them.")
+
+if __name__ == "__main__":
+    main()
